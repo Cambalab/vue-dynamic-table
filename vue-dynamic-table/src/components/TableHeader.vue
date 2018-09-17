9cd0a5c96a7151b2f62255f55f1635b04ca5217d
Leonardo Vaquel <leonardovaquel@gmail.com>
2018-09-17 19:01:55 -0300

    Added new component TableHeader.vue

---
+++ vue-dynamic-table/src/components/TableHeader.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <div class="" v-for="column in this.columnsHeader">
+    <div class="">
+      <label>{{ replaceUnderscores(column.text) }}</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    columnsHeader: {
+      type: Array,
+      requiered: true
+    }
+  },
+  methods: {
+    replaceUnderscores(text) {
+      return text.replace(/[_-]/g, ' ')
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
