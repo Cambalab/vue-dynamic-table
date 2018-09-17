7f7d83c07cb6257e6768216349e81c7386617188
Leonardo Vaquel <leonardovaquel@gmail.com>
2018-09-17 19:12:36 -0300

    Added new component DeleteRowButton.vue

---
+++ vue-dynamic-table/src/components/DeleteRowButton.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+  <div class="">
+    <i v-if="this.showMinusButton"
+      :class="this.typeButtonClass"
+      v-on:click="deleteRow()"
+      data-toggle="tooltip"
+      :data-original-title="this.tooltipTitle">
+    </i>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    showMinusButton: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    typeButtonClass: {
+      type: String,
+      required: false,
+      default: 'fa fa-trash-o x2'
+    },
+    tooltipTitle: {
+      type: String,
+      required: false,
+      default: 'Delete'
+    }
+  },
+  methods: {
+    deleteRow() {
+      this.$emit('deleteRow')
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
