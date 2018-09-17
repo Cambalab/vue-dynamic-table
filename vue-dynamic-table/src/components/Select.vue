533ab1fa5c46801df14dddc6357c861e91158316
Leonardo Vaquel <leonardovaquel@gmail.com>
2018-09-17 18:22:53 -0300

    Added new component Select.vue

---
+++ vue-dynamic-table/src/components/Select.vue
@@ -0,0 +1,64 @@
+<template lang="html">
+  <select class="form-control" v-model="selectedOption" @change="selectOption(selectedOption)">
+    <option v-bind:value="null">{{this.placeholder}}</option>
+    <option v-for="option in OptionsList" class="item-text" v-bind:value="option.id">{{option.text}}</option>
+  </select>
+</template>
+<script>
+export default {
+  
+  name: 'select',
+  data() {
+    return {
+      selectedOption: null
+    }
+  },
+  props: {
+    OptionsList: {
+      type: Array,
+      required: true
+    },
+    selected: {
+      type: Number
+    },
+    index: {
+      type: Number
+    },
+    placeholder: {
+      type: String,
+      required: false,
+      default: 'Select an option'
+    },
+    fetchedValue: {
+      type: Number,
+      required: false
+    }
+  },
+  watch: {
+    selected: function(newVal, oldVal) {
+      this.selectedOption = newVal
+    },
+    fetchedValue: function(newVal, oldVal) {
+      this.selectOption(this.fetchedValue)
+    }
+  },
+  methods: {
+    selectOption(currentOption) {
+      this.$emit('selectOption', { selectedOption: currentOption, index: this.index })
+    },
+    refreshData() {
+      if (this.fetchedValue) {
+        this.selectedOption = this.fetchedValue
+      }
+    }
+  },
+  created() {
+    this.refreshData()
+  }
+}
+</script>
+<style lang="css">
+select{
+  cursor: pointer;
+}
+</style>
