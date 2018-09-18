75b43672f71adb3d411ee3ccff126b5fb42bd4b7
Leonardo Vaquel <leonardovaquel@gmail.com>
2018-09-17 22:14:07 -0300

    Added new component AddRowButton.vue

---
+++ vue-dynamic-table/src/components/AddRowButton.vue
@@ -0,0 +1,38 @@
+<template lang="html" v-if="this.showPlusButton">
+  <div class="" @click="addNewRow()">
+    <i  :class="this.typeButtonClass"
+        data-toggle="tooltip"
+        :data-original-title="this.tooltipTitle">
+    </i>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    typeButtonClass: {
+      type: String,
+      required: false,
+      default: 'fa fa-plus-square-o x2'
+    },
+    tooltipTitle: {
+      type: String,
+      required: false,
+      default: 'Add row'
+    },
+    showPlusButton: {
+      type: Boolean,
+      required: false,
+      default: true
+    }
+  },
+  methods: {
+    addNewRow() {
+      this.$emit('addNewRow')
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
